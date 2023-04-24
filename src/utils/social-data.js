545fad0c7ac4556a3e073cbf6c4a443aeface6ef
Dart18-80 <diegoruiztrabanino13@gmail.com>
2023-04-24 11:06:20 -0600

    first commit

---
+++ src/utils/social-data.js
@@ -0,0 +1,18 @@
+export const socialData = [
+  {
+    type: "youtube",
+    link: "https://www.youtube.com/@EladioCarrion",
+  },
+  {
+    type: "twitter",
+    link: "https://twitter.com",
+  },
+  {
+    type: "facebook",
+    link: "https://www.facebook.com/profile.php?id=100005910097628&locale=es_LA",
+  },
+  {
+    type: "linkedin",
+    link: "https://www.linkedin.com/in/diego-ruiz-29ab7a230",
+  },
+];
